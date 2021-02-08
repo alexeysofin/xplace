dd9b7bbb9d73303ae0b230f4df3437c5ebd49014
Aliaksei Sofin <sofin.moffin@gmail.com>
2021-02-08 13:06:04 +0300

    Add backup cron

--- xplace/xplace/celery.py
+++ xplace/xplace/celery.py
@@ -4,17 +4,20 @@ import os
 import raven
 from raven.contrib.celery import register_signal, register_logger_signal
 
+from xplace.compute import tasks
+
 from celery import Celery as CeleryBase
+from celery.schedules import crontab
 
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xplace.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xplace.settings")
 
 
 class Celery(CeleryBase):
     def on_configure(self):
-        client = raven.Client(dsn=settings.RAVEN_CONFIG.get('dsn'))
+        client = raven.Client(dsn=settings.RAVEN_CONFIG.get("dsn"))
 
         # register a custom filter to filter out duplicate logs
         register_logger_signal(client)
@@ -23,13 +26,23 @@ class Celery(CeleryBase):
         register_signal(client)
 
 
-app = Celery('xplace')
+app = Celery("xplace")
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Executes every Monday morning at 0:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=0, minute=30, day_of_week=1),
+        tasks.backup_all_containers.s(),
+    )
+
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
